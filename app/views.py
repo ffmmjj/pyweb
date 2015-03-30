494d74d00d73b4dae45a4b16a0205c05f4e91efa
Jessica Rocha <jessicarocha.anjos@gmail.com>
2015-03-30 19:27:05 -0300

    001 [Jessica] Added logout button and logic

--- app/views.py
+++ app/views.py
@@ -12,25 +12,34 @@ def index():
     if user == None:
         return redirect('/login',302)
     else:
+        user = dict(nickname=session.get('user_nickname'))
         return render_template("index.html",
                                title='Home',
                                user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    users_query = g.db.execute('select id, username, password from users')
-    users = [dict(id=row[0], username=row[1], password=row[2]) for row in users_query.fetchall()]
+    if get_current_user() is None:
+        error = None
+        users_query = g.db.execute('select id, username, password from users')
+        users = [dict(id=row[0], username=row[1], password=row[2]) for row in users_query.fetchall()]
 
-    if request.method == 'POST':
-        if request.form['username'] not in [user['username'] for user in users]:
-            error = 'Invalid username or password'
-        elif request.form['password'] != [user['password'] for user in users if user['username'] == request.form['username']][0]:
-            error = 'Invalid username or password'
-        else:
-            session['logged_in'] = True
-            session['user_nickname'] = request.form['username']
-            user = dict(nickname=session.get('user_nickname'))
-            return render_template('index.html', user=user)
+        if request.method == 'POST':
+            if request.form['username'] not in [user['username'] for user in users]:
+                error = 'Invalid username or password'
+            elif request.form['password'] != [user['password'] for user in users if user['username'] == request.form['username']][0]:
+                error = 'Invalid username or password'
+            else:
+                session['logged_in'] = True
+                session['user_nickname'] = request.form['username']
+                return redirect(url_for('index'))
 
-    return render_template('login.html', error=error)
+        return render_template('login.html', error=error)
+
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
