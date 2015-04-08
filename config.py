3e349c7b1a139847c9094fe7e9d90fedbde5d1e2
Abraao Alcantara <sephioh@gmail.com>
2015-04-08 16:53:09 -0300

    012 [abraao] added config variables for mongodb

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+MONGODB_DB = 'sweet_database'
+# MONGODB_HOST = '192.168.1.35'
+# MONGODB_PORT = 12345
+# MONGODB_USERNAME = 'webapp'
+# MONGODB_PASSWORD = 'pwd123'
